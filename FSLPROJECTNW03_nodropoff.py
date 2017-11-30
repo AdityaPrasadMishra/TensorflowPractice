246bbe91d8c0fbdd6fc5329ab4dc06d8c3912b82
Aditya Prasad Mishra <amishr28@mainex1.asu.edu>
2017-11-30 08:04:48 -0700

    change in struct

--- FSLPROJECTNW03_nodropoff.py
+++ FSLPROJECTNW03_nodropoff.py
@@ -6,19 +6,8 @@ Created on Sun Nov 26 12:46:25 2017
 @author: aditya
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 25 08:40:09 2017
-
-@author: aditya
-"""
-
-
-import argparse
-import sys
-import tempfile
 import math
+import os
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -32,7 +21,8 @@ MNIST_IMAGE_PIXELS =28*28
 OUTPUT_CLASSES = 10 
 Batch_Size = 100
 LEARNING_RATE = 0.01
-hiddenlayer_units =128
+hiddenlayer_units =32
+expno = "1"
 
 def deepnnwithrelu(images):  
   """deepnn builds the graph for a deep net for classifying digits.
@@ -307,44 +297,61 @@ def appstart(stri):
     correct_prediction = tf.cast(correct_prediction, tf.float32)
   accuracy = tf.reduce_mean(correct_prediction)
 
-  graph_location = tempfile.mkdtemp()
+  graph_location = "tfgraphs/"+expno
   print('Saving graph to: %s' % graph_location)
   train_writer = tf.summary.FileWriter(graph_location)
   train_writer.add_graph(tf.get_default_graph())
   resultarray =[]
   iterarray=[]
   accarray=[]
+  testaccarray = []
 
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(40000):
+    for i in range(20000):
       batch = mnist.train.next_batch(50)
       if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
             x: batch[0], y_: batch[1]})
-        print('step %d, training accuracy %g' % (i, train_accuracy))
+        testaccuracy = accuracy.eval(feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels})
+        #print('step %d, training accuracy %g' % (i, train_accuracy))
+        #print('test accuracy %g' %testaccuracy)
         iterarray.append(i)
         accarray.append(train_accuracy)
+        testaccarray.append(testaccuracy)
       train_step.run(feed_dict={x: batch[0], y_: batch[1]})
     resultarray.append(iterarray)
     resultarray.append(accarray)
-    
-    testaccuracy = accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels})    
-    print('test accuracy %g' %testaccuracy)
-    resultarray.append(testaccuracy)
+    resultarray.append(testaccarray)
   return resultarray  
 
-rarray =[]
-rarray.append(appstart("sigmoid"))
-rarray.append(appstart("relu"))
-rarray.append(appstart("elu"))
-fig = plt.figure()
-axes = fig.add_axes([0.1,0.1,0.8,0.8])
-axes.plot(rarray[0][0],rarray[0][1],'r')
-axes.plot(rarray[0][0],rarray[1][1],'b')
-axes.plot(rarray[0][0],rarray[2][1],'g')
-
-axes.set_xlabel('Train Iterations')
-axes.set_ylabel('Train accuracy')
-plt.plot()
+def progstart():
+    rarray =[]
+    rarray.append(appstart("sigmoid"))
+    rarray.append(appstart("relu"))
+    rarray.append(appstart("elu"))
+    if not os.path.exists('figures'):
+        os.makedirs('figures')
+    fig1 = plt.figure()
+    axes1 = fig1.add_axes([0.1,0.1,0.8,0.8])
+    axes1.plot(rarray[0][0],rarray[0][1],'r')
+    axes1.plot(rarray[0][0],rarray[1][1],'b')
+    axes1.plot(rarray[0][0],rarray[2][1],'g')
+    
+    axes1.set_xlabel('Train Iterations')
+    axes1.set_ylabel('Train accuracy')
+    fig1.savefig('figures/'+expno+'_trainAccuracy.png')
+    
+    fig2 = plt.figure()
+    axes2 = fig2.add_axes([0.1,0.1,0.8,0.8])
+    axes2.plot(rarray[0][0],rarray[0][2],'r')
+    axes2.plot(rarray[0][0],rarray[1][2],'b')
+    axes2.plot(rarray[0][0],rarray[2][2],'g')
+    fig2.savefig('figures/'+expno+'_testAccuracy.png')
+    
+    axes2.set_xlabel('Train Iterations')
+    axes2.set_ylabel('Test accuracy')
+    plt.plot()
+    
+progstart()
---
+++ mainfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 03:15:27 2017
+
+@author: aditya
+"""
+
+import Exp1
+import Exp2
+import Exp3
+import Exp4
+import Exp5
+import Exp6
+
+Exp1.progstart()
+Exp2.progstart()
+Exp3.progstart()
+Exp4.progstart()
+Exp5.progstart()
+Exp6.progstart()
+
